0c2759d5117af7f97bbf0dea657fc2b286242067
Marcus <marek17.10.2001@gmail.com>
2023-10-31 23:43:31 +0100

    Refactoring, change concept

--- multiplicationBinaryShort.js
+++ multiplicationBinaryShort.js
@@ -1,22 +1,29 @@
-let a = 10000;
-let b = 5;
-let tab = [];
-let text = "";
-tab.push(a);
-tab.push(b);
-console.log(String(tab[0]));
-for (let i = 0; i < tab.length; i++) {
-  text = tab[i];
-  console.log(String(tab[i]).length);
-  console.log(tab[i + 1]);
-  for (let j = 0; j < parseInt(text).length; j++) {
-    if (text[i] == "1" || text[i] == "0") {
-      console.log("Binary");
-    } else {
-      console.log("Nie binary");
-    }
-  }
+let binaryValueFirst = "10";
+let binaryValueSecond = "5";
+
+let convertedArray = convertToNumber(binaryValueFirst, binaryValueSecond);
+let resultMultiplication = multiplication(convertedArray);
+convertToBinarySystem(binaryValueFirst, binaryValueSecond);
+console.log(convertToBinarySystem(resultMultiplication));
+
+function convertToBinarySystem(resultMultiplication) {
+  return resultMultiplication.toString(2);
 }
-console.log(text.length);
-let wynik = a * b;
-console.log(wynik.toString(2));
+
+function convertToNumber(binaryValueFirst, binaryValueSecond) {
+  let arr = [binaryValueFirst, binaryValueSecond];
+  for (let i = 0; i < 2; i++) {
+    arr[i] = Number(arr[i]);
+  }
+  return arr;
+}
+
+function multiplication(convertedArray) {
+  let result = 1;
+  for (let i = 0; i < convertedArray.length; i++) {
+    result *= convertedArray[i];
+  }
+  return result;
+}
+
+// pozwolic uzytkownikowi na wprowadzenie wiecej niz dwoch wartosci
