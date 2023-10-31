627e4ab1d0c8a7e0c3352e8e03efcfa60b99ee6d
Marcus <marek17.10.2001@gmail.com>
2023-10-31 23:58:40 +0100

    Added addition and substraction files

---
+++ additionBinaryShort.js
@@ -0,0 +1,28 @@
+let binaryValueFirst = "10";
+let binaryValueSecond = "5";
+
+let convertedArray = convertToNumber(binaryValueFirst, binaryValueSecond);
+let resultAddition = Addition(convertedArray);
+
+convertToBinarySystem(binaryValueFirst, binaryValueSecond);
+console.log(convertToBinarySystem(resultAddition));
+
+function convertToBinarySystem(resultAddition) {
+  return resultAddition.toString(2);
+}
+
+function convertToNumber(binaryValueFirst, binaryValueSecond) {
+  let arr = [binaryValueFirst, binaryValueSecond];
+  for (let i = 0; i < 2; i++) {
+    arr[i] = Number(arr[i]);
+  }
+  return arr;
+}
+
+function Addition(convertedArray) {
+  let result = 0;
+  for (let i = 0; i < convertedArray.length; i++) {
+    result += convertedArray[i];
+  }
+  return result;
+}
