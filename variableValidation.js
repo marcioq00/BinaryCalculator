508ea219c352842490dd20cd25c89bdd20019400
Marcus <marek17.10.2001@gmail.com>
2023-11-04 01:21:27 +0100

    Formating code

--- variableValidation.js
+++ variableValidation.js
@@ -1,13 +1,4 @@
 export function validateNegativeNumbers(binaryValueFirst, binaryValueSecond) {
-  /*try {
-    if (!(binaryValueFirst <= 0 || binaryValueSecond <= 0)) {
-      let arr = [binaryValueFirst, binaryValueSecond];
-      return arr;
-    }
-  } catch (error) {
-    console.error(error);
-  }*/
-
   if (binaryValueFirst <= 0 || binaryValueSecond <= 0) {
     throw new Error("Jedna z wartości jest ujemna.");
   }
